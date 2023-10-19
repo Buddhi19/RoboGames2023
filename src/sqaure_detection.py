53b00088d884b3f25d4176ce4cceec4de2196812
DMUPraveen <dmu.praveen@gmail.com>
2023-10-19 21:38:45 +0530

    finding centers

--- src/sqaure_detection.py
+++ src/sqaure_detection.py
@@ -3,6 +3,15 @@ import numpy as np
 bgr = cv2.imread("new.jpg")
 gray = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
 
+
+def get_square_center(contour):
+    M = cv2.moments(contour)
+    cx = int(M['m10']/M['m00'])
+    cy = int(M['m01']/M['m00'])
+    area = cv2.contourArea(contour)
+    return (cx, cy, area)
+
+
 while True:
     ret, thresh = cv2.threshold(gray, 150, 255, 0)
     contours, _ = cv2.findContours(
@@ -12,7 +21,11 @@ while True:
     quadrilaterals = list(filter(lambda x: 4 <= len(x) <= 5, approx))
     large_quads = list(
         filter(lambda quad: cv2.contourArea(quad) > 50, quadrilaterals))
+    centers = [get_square_center(quad) for quad in large_quads]
+
     drawn = cv2.drawContours(bgr, large_quads, -1, (0, 255, 0), 3)
+    for cx, cy, a in centers:
+        drawn = cv2.circle(drawn, (cx, cy), 5, (255, 0, 0), -1)
     cv2.imshow("debug", drawn)
     cv2.waitKey(1)
 
