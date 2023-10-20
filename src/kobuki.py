d5fffb65f1037e489b20841fc8596d578738ca95
Buddhi Wijenayake <wijenayakebuddhi34802@gmail.com>
2023-10-20 11:37:49 +0530

    square class

--- src/kobuki.py
+++ src/kobuki.py
@@ -1,18 +1,58 @@
 import KobukiDriver as kobuki
 import time
-from camera import Circle
+import cv2
+import freenect
+from circle_workflows import Circle_Workflow, Hugh_circle_Workflow
+from ball_finder_machine import Ball_Finder_Machine
+from Robot_Vision import RobotVision
+from computer_vision import Color_Filter_HSV, Combined_Filter
+from sqaure_detection import Square
+import numpy as np
 
+class Red:
+    hl = 0
+    hh = 63
+    sl = 88
+    sh = 208
+
+
+class Blue:
+    hl = 112
+    hh = 132
+    sl = 54
+    sh = 255
+
+def get_depth_and_bgr():
+    depth, timestamp = freenect.sync_get_depth()
+    rgb, timestamp = freenect.sync_get_video()
+    depth = cv2.cvtColor(depth, cv2.COLOR_GRAY2BGR)
+    bgr = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
+    return depth, bgr
+
+def check_ball(bgr,filter):
+    sampled_image=bgr[300:480, :540]
+    filtered_image = filter.get_mask_for_bgra(bgr)
+    return filtered_image
 
 def main():
     my_kobuki = kobuki.Kobuki()
-    circle = Circle()
-    # Play start up sound
-    #   my_kobuki.play_on_sound()
+    filr = Color_Filter_HSV(Red.hl, Red.hh, Red.sl, Red.sh)
+    filb = Color_Filter_HSV(Blue.hl, Blue.hh, Blue.sl, Blue.sh)
+    fil = Combined_Filter([filr, filb])
 
+
+    cw = Hugh_circle_Workflow(fil)
+    sq = Square()
+    i=0
     while True:
+        bgr=get_depth_and_bgr()[1]
+        filtered_image = fil.get_mask_for_bgra(bgr)
+        circle, dilated_image, gray_masked_image = cw.find_circles(bgr)
+        drawn = cw.draw_circles(circle, bgr)
+        cv2.imshow("debug", drawn)
+        cv2.waitKey(1)
+
         key = input("Enter command: ")
-        if key=="c":
-            circle.identify_circles()
         if key == "w":
             # Move forward
             my_kobuki.move(200, 200, 0)
@@ -21,10 +61,10 @@ def main():
             my_kobuki.move(-200, -200, 0)
         elif key == "a":
             # Turn left
-            my_kobuki.move(100, -100, 0)
+            my_kobuki.move(200, -200, 0)
         elif key == "d":
             # Turn right
-            my_kobuki.move(-100, 100, 0)
+            my_kobuki.move(-200, 200, 0)
         elif key == "x":
             # Stop
             my_kobuki.move(0, 0, 0)
@@ -39,12 +79,17 @@ def main():
             time.sleep(1)
             my_kobuki.clr_led1()
             my_kobuki.clr_led2()
+
+        elif key=="c":
+            cv2.imwrite(f"image{i}.png",bgr)
+            i+=1
         elif key == "q":
             # Quit
             break
 
         # Print sensor data
-        print(my_kobuki.encoder_data())
+        # print(my_kobuki.encoder_data())
+        print(my_kobuki.inertial_sensor_data())
 
 
 if __name__ == "__main__":
--- src/sqaure_detection.py
+++ src/sqaure_detection.py
@@ -3,30 +3,31 @@ import numpy as np
 bgr = cv2.imread("new.jpg")
 gray = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
 
+class Square:
+    def get_square_center(self,contour):
+        M = cv2.moments(contour)
+        cx = int(M['m10']/M['m00'])
+        cy = int(M['m01']/M['m00'])
+        area = cv2.contourArea(contour)
+        return (cx, cy, area)
 
-def get_square_center(contour):
-    M = cv2.moments(contour)
-    cx = int(M['m10']/M['m00'])
-    cy = int(M['m01']/M['m00'])
-    area = cv2.contourArea(contour)
-    return (cx, cy, area)
 
+    def find_squares(self):
+        ret, thresh = cv2.threshold(gray, 150, 255, 0)
+        contours, _ = cv2.findContours(
+            thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        approx = [cv2.approxPolyDP(
+            cnt, 0.015*cv2.arcLength(cnt, True), True) for cnt in contours]
+        quadrilaterals = list(filter(lambda x: 4 <= len(x) <= 5, approx))
+        large_quads = list(
+            filter(lambda quad: cv2.contourArea(quad) > 50, quadrilaterals))
+        centers = [self.get_square_center(quad) for quad in large_quads]
 
-while True:
-    ret, thresh = cv2.threshold(gray, 150, 255, 0)
-    contours, _ = cv2.findContours(
-        thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    approx = [cv2.approxPolyDP(
-        cnt, 0.015*cv2.arcLength(cnt, True), True) for cnt in contours]
-    quadrilaterals = list(filter(lambda x: 4 <= len(x) <= 5, approx))
-    large_quads = list(
-        filter(lambda quad: cv2.contourArea(quad) > 50, quadrilaterals))
-    centers = [get_square_center(quad) for quad in large_quads]
+        drawn = cv2.drawContours(bgr, large_quads, -1, (0, 255, 0), 3)
+        for cx, cy, a in centers:
+            drawn = cv2.circle(drawn, (cx, cy), 5, (255, 0, 0), -1)
+        cv2.imshow("debug", drawn)
+        # cv2.waitKey(1)
+        return centers
 
-    drawn = cv2.drawContours(bgr, large_quads, -1, (0, 255, 0), 3)
-    for cx, cy, a in centers:
-        drawn = cv2.circle(drawn, (cx, cy), 5, (255, 0, 0), -1)
-    cv2.imshow("debug", drawn)
-    cv2.waitKey(1)
-
-cv2.destroyAllWindows()
+# cv2.destroyAllWindows()
